ebe3472add5cc3bfcb03f61786e09f3742cc5b8a
Machado Avitia Manuel Enrique <15325061080136@cetis108.edu.mx>
2017-04-05 23:03:21 -0600

    v2

--- app/src/main/java/mx/edu/cetis108/cetis1084am_15325061080136_app004/MapsActivity.java
+++ app/src/main/java/mx/edu/cetis108/cetis1084am_15325061080136_app004/MapsActivity.java
@@ -1,18 +1,50 @@
 package mx.edu.cetis108.cetis1084am_15325061080136_app004;
 
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.location.LocationListener;
 
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
+    public Marker marcador;
+    double lat = 0.0;
+    double lng = 0.0;
+    LocationListener loctionListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+
+        }
+
+        @Override
+        public void onStatusChanged(String s, int i, Bundle bundle) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String s) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String s) {
+
+        }
+    };
     private GoogleMap mMap;
 
     @Override
@@ -38,11 +70,34 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        miUbicacion();
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(25.5777234,-108.4762999);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("15325061080136_Manuel Machado"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+    public void agregarMarcador(double lat, double lng) {
+        LatLng coordenadas = new LatLng(lat, lng);
+        CameraUpdate miUbicacion = CameraUpdateFactory.newLatLngZoom(coordenadas, 16);
+        if (marcador != null) marcador.remove();
+        marcador = mMap.addMarker(new MarkerOptions().position(coordenadas).title("Mi posición Actual").icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_launcher)));
+        mMap.animateCamera(miUbicacion);
+    }
 
+    public void actualizarUbicacion(Location location) {
+        if (location != null) {
+            lat = location.getLatitude();
+            lng = location.getLongitude();
+            agregarMarcador(lat, lng);
+        }
+    }
+
+    public void miUbicacion()
+    {
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+        {
+            return;
+        }
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        actualizarUbicacion(location);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,15000,0, loctionListener);
     }
 }
